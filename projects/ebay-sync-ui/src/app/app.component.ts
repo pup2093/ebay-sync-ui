00c1790013fde4964367065af45431e289c232bc
pup2093 <45723277+pup2093@users.noreply.github.com>
2024-04-15 16:10:50 -0400

    scaffold workspace

---
+++ projects/ebay-sync-ui/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+
+@Component({
+  selector: 'esync-root',
+  standalone: true,
+  imports: [MainLayoutComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'ebay-sync-ui';
+}
