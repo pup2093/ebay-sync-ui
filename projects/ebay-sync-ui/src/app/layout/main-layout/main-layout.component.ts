ecc80c1ca9476c84a21baf471d85dc7407880349
pup2093 <45723277+pup2093@users.noreply.github.com>
2024-05-05 14:58:05 -0400

    add logout functionality

--- projects/ebay-sync-ui/src/app/layout/main-layout/main-layout.component.ts
+++ projects/ebay-sync-ui/src/app/layout/main-layout/main-layout.component.ts
@@ -4,6 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { OAuthService } from 'angular-oauth2-oidc';
 @Component({
   selector: 'esync-main-layout',
   standalone: true,
@@ -17,4 +18,10 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  constructor(private oauthService: OAuthService) {}
+
+  logout(): void {
+    this.oauthService.logOut();
+  }
+}
