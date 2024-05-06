c3aadb060dd3be37d09aec2a04376464a89a267e
pup2093 <45723277+pup2093@users.noreply.github.com>
2024-05-05 20:38:47 -0400

    add sample http call with bearer token

--- projects/ebay-sync-ui/src/app/app.component.ts
+++ projects/ebay-sync-ui/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { HttpClient } from '@angular/common/http';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'esync-root',
   standalone: true,
-  imports: [MainLayoutComponent],
+  imports: [MainLayoutComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ebay-sync-ui';
+  product: unknown = undefined;
+
+  constructor(
+    private http: HttpClient,
+    private oauthService: OAuthService,
+  ) {}
+
+  //sample api call with bearer token
+  // getProduct() {
+  //   this.http.get<unknown>('http://localhost:8080/api/v1/product/1', {
+  //     headers: {
+  //       'Authorization': `Bearer ${this.oauthService.getAccessToken()}`
+  //     }
+  //   }).subscribe((val) => this.product = val );
+  // }
 }
