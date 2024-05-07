4ecfd71d903fd4bb865c4397bd4d6c66e3f4877e
pup2093 <45723277+pup2093@users.noreply.github.com>
2024-05-07 14:54:45 -0400

    add sync settings

--- projects/ebay-sync-ui/src/app/feature/sync/sync.component.ts
+++ projects/ebay-sync-ui/src/app/feature/sync/sync.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../ui/card/card.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'esync-sync',
   standalone: true,
-  imports: [CardComponent, MatSlideToggleModule],
+  imports: [
+    CardComponent,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './sync.component.html',
   styleUrl: './sync.component.scss',
 })
 export class SyncComponent {
-  toggleText = 'Sync Off';
+  readonly SYNC_ON = 'Sync on';
+  readonly SYNC_OFF = 'Sync off';
+  toggleText = this.SYNC_ON;
+  readOnly = true;
 
   toggleChange() {
-    this.toggleText = this.toggleText === 'Sync Off' ? 'Sync On' : 'Sync Off';
+    this.toggleText =
+      this.toggleText === this.SYNC_OFF ? this.SYNC_ON : this.SYNC_OFF;
   }
 }
