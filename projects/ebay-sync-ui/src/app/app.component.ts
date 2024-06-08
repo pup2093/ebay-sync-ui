569d63a8a5741f1e34770e83421437012b8a3db9
pup2093 <45723277+pup2093@users.noreply.github.com>
2024-06-08 08:39:17 -0400

    clean up

--- projects/ebay-sync-ui/src/app/app.component.ts
+++ projects/ebay-sync-ui/src/app/app.component.ts
@@ -1,29 +1,15 @@
 import { Component } from '@angular/core';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
-import { HttpClient } from '@angular/common/http';
-import { OAuthService } from 'angular-oauth2-oidc';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'esync-root',
   standalone: true,
-  imports: [MainLayoutComponent, JsonPipe],
+  imports: [MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ebay-sync-ui';
-  product: unknown = undefined;
 
-  constructor(
-    private http: HttpClient,
-    private oauthService: OAuthService,
-  ) {}
-
-  //sample api call with bearer token
-  getProduct() {
-    this.http
-      .get<unknown>('http://localhost:8080/api/v1/userprofile')
-      .subscribe((val) => (this.product = val));
-  }
+  constructor() {}
 }
