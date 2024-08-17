e8204e2ae747be69721006010064a671a6dc07ae
pup2093 <45723277+pup2093@users.noreply.github.com>
2024-08-17 11:29:24 -0400

    add prod urls to environment

--- projects/ebay-sync-ui/src/environments/environment.ts
+++ projects/ebay-sync-ui/src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  authIssuerUrl: '',
-  authTokenUrl: '',
-  serverBaseUrl: '',
+  authIssuerUrl:
+    'https://keycloak-production-9908.up.railway.app/realms/ebay-sync-prod',
+  authTokenUrl:
+    'https://keycloak-production-9908.up.railway.app/realms/ebay-sync-prod/protocol/openid-connect/token',
+  serverBaseUrl: 'https://ebay-sync-server-production.up.railway.app/api/v1/',
 };
